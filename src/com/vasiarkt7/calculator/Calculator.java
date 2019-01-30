b03ef85c4aa1b0bcf6d9007c4880e7790edb2311
vasiarkt7 <vasiarkt7@gmail.com>
2019-01-30 19:19:17 +0200

    master - change project structure

---
+++ src/com/vasiarkt7/calculator/Calculator.java
@@ -0,0 +1,7 @@
+package com.vasiarkt7.calculator;
+
+class Calculator {
+    int calculateSum(int operandFirst, int operandSecond) {
+        return operandFirst + operandSecond;
+    }
+}
---
+++ src/com/vasiarkt7/calculator/Main.java
@@ -0,0 +1,8 @@
+package com.vasiarkt7.calculator;
+
+public class Main {
+    public static void main(String args[]) {
+        Calculator result = new Calculator();
+        System.out.println(result.calculateSum(12, 20));
+    }
+}
