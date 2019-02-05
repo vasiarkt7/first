76c4fa0c1d9984d28a61d4405cd3f66be90ac7db
vasiarkt7 <vasiarkt7@gmail.com>
2019-02-05 14:18:49 +0200

    add_operand_dividing_substraction_multiplieng_operations - Calculator - add operation  methods - add maven framework

--- src/com/vasiarkt7/calculator/Calculator.java
+++
@@ -1,7 +0,0 @@
-package com.vasiarkt7.calculator;
-
-class Calculator {
-    int calculateSum(int operandFirst, int operandSecond) {
-        return operandFirst + operandSecond;
-    }
-}
---
+++ src/main/java/com/vasiarkt7/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package com.vasiarkt7.calculator;
+
+class Calculator {
+    int calculateSum(int leftOperand, int rightOperand) {
+        return leftOperand + rightOperand;
+    }
+
+    int calculateSubstraction(int leftOperand, int rightOperand) {
+        return leftOperand + rightOperand;
+    }
+
+    int calculateDividing(int leftOperand, int rightOperand) {
+        return leftOperand + rightOperand;
+    }
+
+    int calculateMultiplieng(int leftOperand, int rightOperand) {
+        return leftOperand + rightOperand;
+    }
+}
